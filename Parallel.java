0f5bc2626ab5f25e3ce594af77a52a68d3cc8639
David <david@linux.(none)>
2010-05-25 15:58:01 +0200

    whiteBalance

--- Parallel.java
+++ Parallel.java
@@ -34,6 +34,8 @@ public class Parallel implements Runnable {
 		Thread t1 = new Thread(new Parallel(c1,1));
 		Thread t2 = new Thread(new Parallel(c2,2));
 		
+		// BlackBalance
+
 		System.out.println("auf Schwarz stellen und Enter");
 		
 		Button.ENTER.waitForPress();
@@ -43,7 +45,22 @@ public class Parallel implements Runnable {
 		LCD.clear();
 		System.out.println("Kalibriert bitte Enter");
 		Button.ENTER.waitForPress();
+
+		// Clearen
+
+		LCD.clear();
+
+		// WhiteBalance
+
+		System.out.println("auf Weiss stellen und Enter");
 		
+		Button.ENTER.waitForPress();
+		c1.initWhiteBalance();
+		c2.initWhiteBalance();
+		
+		LCD.clear();
+		System.out.println("Kalibriert bitte Enter");
+		Button.ENTER.waitForPress();
 		
 		t1.start();
 		t2.start();
