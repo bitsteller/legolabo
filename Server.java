1b2b0a6fd3dbb9b3d877e524e6bf90083bfe51b4
Nils B <nbnet@arcor.de>
2010-05-26 17:07:03 +0200

    bugfix

--- Server.java
+++ Server.java
@@ -3,13 +3,8 @@ import lejos.pc.comm.*;
 public class Server {
 	public static void main(String[] args) throws Exception {
 		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
-		//NXTInfo[] nxtInfo = nxtComm.search("IPS_05",NXTCommFactory.BLUETOOTH);
-		
-		/*for (NXTInfo info:nxtInfo) {
-			System.out.println(info.name + ": " + info.deviceAddress);
-		}*/
-		
-		NXTInfo nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "IPS_05", "00:16:53:02:A5:1F");
+		NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "IPS_05", "00:16:53:02:A5:1F");
+		nxtComm.open(info);
 		
 	}
 
