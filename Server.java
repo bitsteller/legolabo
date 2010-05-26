73d07a651f7128fd3a4a80b5ac1c9096a6f29fe6
Nils B <nbnet@arcor.de>
2010-05-26 15:44:32 +0200

    Server

---
+++ Server.java
@@ -0,0 +1,14 @@
+import lejos.pc.comm.*;
+
+public class Server {
+	public static void main(String[] args) {
+		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
+		NXTInfo[] nxtInfo = nxtComm.search("NXT",NXTCommFactory.BLUETOOTH);
+		
+		for (NXTInfo info:nxtInfo) {
+			System.out.println(info.name + ": " + info.deviceAddress);
+		} 
+		
+	}
+
+}
