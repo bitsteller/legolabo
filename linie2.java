1798e207ad222da03d347c0c630f7bf3baa51b61
David <david@linux.(none)>
2010-05-26 11:46:52 +0200

    er fährt

--- linie2.java
+++ linie2.java
@@ -6,7 +6,9 @@ public class linie2 {
 	public static void run() {
 		while(!Button.ESCAPE.isPressed()){
 			if (Button.ENTER.isPressed()) {
+				Motor.C.stop();
 				moveToNextCrossing();
+				Motor.C.forward();
 				System.out.println("Kreuzung!");
 			}
 		}
@@ -24,7 +26,7 @@ public class linie2 {
 		Motor.B.forward();
 		
 		 
-		while(!Button.ENTER.isPressed()){
+		while(true){
 			int cn1 = c1.getColor()[0] + c1.getColor()[1] + c1.getColor()[2];
 			int cn2 = c2.getColor()[0] + c2.getColor()[1] + c2.getColor()[2];
 			int schwelle = 100;
