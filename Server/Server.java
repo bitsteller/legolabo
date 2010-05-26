32ad2be799914850639c31a0518fa0655e881c03
Nils B <nbnet@arcor.de>
2010-05-26 22:59:07 +0200

    Testing sending string in Server, Recieve string in Client

--- Server/Server.java
+++ Server/Server.java
@@ -1,4 +1,5 @@
 import lejos.pc.comm.*;
+import java.io.*;
 
 public class Server {
 	public static void main(String[] args) throws Exception {
@@ -6,6 +7,15 @@ public class Server {
 		NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "IPS_05", "00:16:53:02:A5:1F");
 		nxtComm.open(info);
 		
+		InputStream is = nxtComm.getInputStream();
+		OutputStream os = nxtComm.getOutputStream();
+		
+		DataInputStream in = new DataInputStream(is);
+		DataOutputStream out = new DataOutputStream(os);
+		
+		out.writeChars("test.");
+		out.flush();
+		out.close();
 	}
 
 }
--- client.java
+++ client.java
@@ -2,14 +2,23 @@ import lejos.nxt.*;
 import lejos.nxt.addon.*;
 import lejos.nxt.remote.*;
 import lejos.nxt.comm.*;
+import java.io.*;
 
 public class client {
 
-	public static void run() {
+	public static void run() throws Exception {
 		NXTConnection connection = Bluetooth.waitForConnection();
+		DataInputStream in = connection.openDataInputStream();
+		
+		char ch = ' ';
+		
+		while(ch != '.') {
+			ch = in.readChar();
+			System.out.print("" + ch);
+		}
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception {
 		ColorSensor c1 = new ColorSensor(SensorPort.S2);
 		ColorSensor c2 = new ColorSensor(SensorPort.S3);
 
