2159824210e04a60d9bba842b22fe2196cbc5407
Nils B <nbnet@arcor.de>
2010-05-26 12:08:24 +0200

    Abbiegen

--- linie2.java
+++ linie2.java
@@ -3,15 +3,18 @@ import lejos.nxt.addon.*;
 
 public class linie2 {
 
+	private static int schwelle = 100;
+
 	public static void run() {
 		while(!Button.ESCAPE.isPressed()){
+			System.out.println("Bitte Enter zum Starten!");
 			if (Button.ENTER.isPressed()) {
 				moveToNextCrossing();
 				System.out.println("Kreuzung!");
+				turn(-1);
+				System.out.println("Links abgebogen!");
 			}
 		}
-
-
 	}
 	
 	public static void moveToNextCrossing() {
@@ -27,7 +30,6 @@ public class linie2 {
 		while(!Button.ENTER.isPressed()){
 			int cn1 = c1.getColor()[0] + c1.getColor()[1] + c1.getColor()[2];
 			int cn2 = c2.getColor()[0] + c2.getColor()[1] + c2.getColor()[2];
-			int schwelle = 100;
 
 			LCD.drawString("Wert1: " + cn1 , 0, 0);
 			LCD.drawString("Wert2: " + cn2 , 0, 2);
@@ -42,6 +44,40 @@ public class linie2 {
 				Motor.B.stop();
 				return;
 			}
+		}
+	}
+	
+	public static void turn(int direction) {
+		ColorSensor c1 = new ColorSensor(SensorPort.S2);
+		ColorSensor c2 = new ColorSensor(SensorPort.S3);
+		
+		Motor.A.setSpeed(150);
+		Motor.B.setSpeed(150);
+		Motor.A.forward();
+		Motor.B.forward();
+		
+		if(isBlack(c1) && isBlack(c2)) {
+			Motor.A.stop();	
+			Motor.B.stop();
+			return;
+		}
+		
+		if (direction == -1) {
+			Motor.B.rotate(60);
+			while(isBlack(c1)){
+				Motor.A.forward();
+			}
+			Motor.A.stop();
+		}
+	}
+	
+	public static boolean isBlack (ColorSensor cs) {
+		int cn = cs.getColor()[0] + cs.getColor()[1] + cs.getColor()[2];
+		if (cn < schwelle) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 	
