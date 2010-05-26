e18e74123aaa0c73de1cc2337a85aec13808cb92
Nils B <nbnet@arcor.de>
2010-05-26 09:40:32 +0200

    linie in linie2 umbenannt und überarbeitet

--- linie2.java
+++ linie2.java
@@ -1,31 +1,31 @@
 import lejos.nxt.*;
 import lejos.nxt.addon.*;
-public class Parallel ruimplements Runnable {
 
-	private ColorSensor cs;
-	
+public class linie2 implements Runnable {
 
 	public void run() {
+		moveToNextCrossing();
+	}
+	
+	public static void moveToNextCrossing() {
 		ColorSensor c1 = new ColorSensor(SensorPort.S2);
 		ColorSensor c2 = new ColorSensor(SensorPort.S3);
 		
 		while(!Button.ENTER.isPressed()){
-			if(c1.getColor
+			if(c1.getColorNumber()!=0) Motor.A.forward();
+			else if(c2.getColorNumber()!=0) Motor.B.forward();
+			else if(c1.getColorNumber()==0) Motor.A.flt();
+			else if(c2.getColorNumber()==0) Motor.B.flt();		
+			
+			if(c1.getColorNumber()==0 && c1.getColorNumber()==0) {
+				Motor.A.stop();	
+				Motor.B.stop();	
+			}
 		}
 	}
-
-	public Parallel(ColorSensor cs, int tNo){
-		this.cs = cs;
-		this.tNo = tNo;
-	}
 	
-	public static void main(String[] args){
-		ColorSensor c1 = new ColorSensor(SensorPort.S2);
-		ColorSensor c2 = new ColorSensor(SensorPort.S3);
-		
-		Thread t1 = new Thread(new Parallel(c1,1));
-		Thread t2 = new Thread(new Parallel(c2,2));
-		
+	public static void kalibrieren(ColorSensor c1, ColorSensor c2) {
+
 		// BlackBalance
 
 		System.out.println("auf Schwarz stellen und Enter");
@@ -53,11 +53,13 @@ public class Parallel ruimplements Runnable {
 		LCD.clear();
 		System.out.println("Kalibriert bitte Enter");
 		Button.ENTER.waitForPress();
-		
-		t1.start();
-		t2.start();
-		
-		System.out.println("Wollen sie beenden?");
-		Button.ENTER.waitForPress();
+
+	}
+	
+	public static void main(String[] args){
+		ColorSensor c1 = new ColorSensor(SensorPort.S2);
+		ColorSensor c2 = new ColorSensor(SensorPort.S3);
+
+		kalibrieren(c1,c2);
 	}
 }
