93811cc80ab7768be2399afe430b36697e51a3df
David <david@linux.(none)>
2010-05-27 11:03:03 +0200

    bla

--- Client/Linie.java
+++ Client/Linie.java
@@ -1,11 +1,11 @@
 import lejos.nxt.*;
 import lejos.nxt.addon.*;
 
-public class linie2 {
+public class Linie {
 
 	public static void run() {
 		while(!Button.ESCAPE.isPressed()){
-				drive.moveToNextCrossing();
+				Drive.moveToNextCrossing();
 				System.out.println("Kreuzung!");
 				Motor.C.forward();
 				Sound.setVolume(75);		//für Wettbewerb 100 setzen
@@ -14,21 +14,21 @@ public class linie2 {
 				while(!Button.ESCAPE.isPressed()){
 					if (Button.RIGHT.isPressed()){
 						System.out.println("Links abbiegen...");
-						drive.turn(-1);
+						Drive.turn(-1);
 						System.out.println("Links abgebogen!");
 						Motor.C.stop();
 						break;
 					}
 					if (Button.LEFT.isPressed()){
 						System.out.println("Rechts abbiegen...");
-						drive.turn(1);
+						Drive.turn(1);
 						System.out.println("Rechts abgebogen!");
 						Motor.C.stop();
 						break;
 					}
 					if (Button.ENTER.isPressed()){
 						System.out.println("Geradeaus fahren...");
-						drive.turn(0);
+						Drive.turn(0);
 						System.out.println("Geradeaus gefahren!");
 						Motor.C.stop();
 						break;
@@ -41,7 +41,7 @@ public class linie2 {
 		ColorSensor c1 = new ColorSensor(SensorPort.S2);
 		ColorSensor c2 = new ColorSensor(SensorPort.S3);
 
-		drive.kalibrieren(c1,c2);
+		Drive.kalibrieren(c1,c2);
 		run();
 	}
 }
