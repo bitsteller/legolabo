78fd140519911cb419fd69ee1471a59aeebb9ffe
David <david@linux.(none)>
2010-05-27 10:06:19 +0200

    MACs vollständig

--- Server/Server.java
+++ Server/Server.java
@@ -4,8 +4,9 @@ import java.io.*;
 public class Server {
 	public static void main(String[] args) throws Exception {
 		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
-		//NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "NXT_05", "00:16:53:02:A5:1F");
+		//NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "IPS_05", "00:16:53:02:A5:1F");
 		NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "NXT_07", "00:16:53:04:ED:8C");
+		NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "NXT_34", "00:16:53:05:94:8F");
 		
 		nxtComm.open(info);
 		
