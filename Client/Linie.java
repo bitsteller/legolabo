7b81f5c4062b91008db057b1b8eb71b8d6a1c3f3
David <david@linux.(none)>
2010-05-27 14:20:17 +0200

    turn

--- Client/Linie.java
+++ Client/Linie.java
@@ -7,30 +7,24 @@ public class Linie {
 		while(!Button.ESCAPE.isPressed()){
 				Drive.moveToNextCrossing();
 				System.out.println("Kreuzung!");
-				Motor.C.forward();
-				Sound.setVolume(75);		//für Wettbewerb 100 setzen
-				Sound.playNote(Sound.FLUTE,440,500);
 				
 				while(!Button.ESCAPE.isPressed()){
 					if (Button.RIGHT.isPressed()){
 						System.out.println("Links abbiegen...");
 						Drive.turn(-1);
 						System.out.println("Links abgebogen!");
-						Motor.C.stop();
 						break;
 					}
 					if (Button.LEFT.isPressed()){
 						System.out.println("Rechts abbiegen...");
 						Drive.turn(1);
 						System.out.println("Rechts abgebogen!");
-						Motor.C.stop();
 						break;
 					}
 					if (Button.ENTER.isPressed()){
 						System.out.println("Geradeaus fahren...");
-						Drive.turn(0);
+						Drive.reverse();
 						System.out.println("Geradeaus gefahren!");
-						Motor.C.stop();
 						break;
 					}
 				}
